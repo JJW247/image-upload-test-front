614f53284b8796eefe87af92e80369b6e322efb7
JJW247 <adr10won@gmail.com>
2021-09-10 17:24:23 +0900

    done

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 const ImageUpload: FC = () => {
   const [file, setFile] = useState<File | null>(null);
+  const [preview, setPreview] = useState<any>(null);
+  const [percent, setPercent] = useState<number>(0);
   const onSubmitImage = async (event: FormEvent<HTMLFormElement>) => {
     try {
       event.preventDefault();
@@ -14,6 +16,12 @@ const ImageUpload: FC = () => {
       const response = await axios.post(
         'http://localhost:3010/image',
         formData,
+        {
+          headers: { 'Content-Type': 'multipart/form-data' },
+          onUploadProgress: (progressEvent) => {
+            setPercent((progressEvent.loaded / progressEvent.total) * 100);
+          },
+        },
       );
       if (response.statusText !== 'Created') {
         throw new Error('알 수 없는 오류가 발생하였습니다!');
@@ -24,16 +32,44 @@ const ImageUpload: FC = () => {
     }
   };
   return (
-    <form onSubmit={onSubmitImage}>
+    <form
+      style={{ width: '100%', textAlign: 'center' }}
+      onSubmit={onSubmitImage}
+    >
+      {preview && (
+        <div style={{ width: '100%', textAlign: 'center' }}>
+          <img style={{ width: '300px' }} src={preview} />
+        </div>
+      )}
+      {percent !== 0 && (
+        <div style={{ width: '100%', marginTop: '30px', marginBottom: '30px' }}>
+          <div
+            style={{
+              width: 100,
+              height: 20,
+              border: '1px solid black',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+          >
+            <div style={{ width: `${percent}%`, backgroundColor: 'red' }}>
+              {percent}%
+            </div>
+          </div>
+        </div>
+      )}
       <input
         type="file"
         onChange={(event) => {
-          if (!event.target.files) {
+          if (!event.target.files || !event.target.files[0]) {
             return;
           }
           const imageFile = event.target.files[0];
           const fileReader = new FileReader();
           setFile(imageFile);
+          fileReader.readAsDataURL(imageFile);
+          fileReader.onload = (readerEvent) =>
+            setPreview(readerEvent.target?.result);
         }}
       />
       <input type="submit" value="업로드" />
